6d2dbf054e63eef0b679e665cb197932426100a6
Uchenna Obicheta <uobicheta@hotmail.com>
2023-04-23 16:56:06 -0400

    Implemented login and logout functionality.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,31 +1,125 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { User, Blog } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
+    // Get all blogs and JOIN with user data
+    const blogData = await Blog.findAll({
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['username'],
         },
       ],
     });
 
     // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
-
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+     console.log(blogs);
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      projects, 
-      logged_in: req.session.logged_in 
+    res.render('home', { 
+      blogs, 
+      logged_in: req.session.logged_in,
+      firstname: req.session.firstname,
+      lastname: req.session.lastname
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+
+
+
+router.get('/blog/:id', withAuth, async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username','user_id'],
+        },
+      ],
+    });
+
+    const blog = blogData.get({ plain: true });
+
+    res.render('add-comment', {
+      ...blog,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/home');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.post('/login', async (req, res) => {
+    try {
+      const userData = await User.findOne({
+        where: {
+          username: req.body.username,
+        },
+      });
+  
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password. Please try again!' });
+        return;
+      }
+      const validPassword = await userData.checkPassword(req.body.password);
+
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password. Please try again!' });
+        return;
+      }
+
+      console.log('You are now logged in!');
+      
+  
+      req.session.save(() => {
+        req.session.firstname = userData.firstname;
+        req.session.lastname = userData.lastname;
+        req.session.logged_in = true;
+        res.status(200).json(userData);   
+      });
+
+     // res
+    //  .status(200)
+    //  .json({ user: dbUserData, message: 'You are now logged in!' });
+
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+router.post('/logout', (req, res) => {
+
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+
+});
+
+/*
 
 router.get('/project/:id', async (req, res) => {
   try {
@@ -78,5 +172,6 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
+*/
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,19 +2,22 @@ const router = require('express').Router();
 
 // const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+/*
 const loginRoutes = require('./loginRoutes');
 const dashboardRoutes = require('./dashboardRoutes');
 const signupRoutes = require('./signupRoutes');
 const blogPostRoutes = require('./blogPostRoutes');
 const commentRoutes = require('./commentRoutes');
+*/
 
 router.use('/', homeRoutes);
 router.use('/home', homeRoutes);
+/*
 router.use('/login', loginRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/signup', signupRoutes);
 router.use('/blogPostRoutes', blogPostRoutes);
 router.use('/commentRoutes', commentRoutes);
-
+*/
 
 module.exports = router;
