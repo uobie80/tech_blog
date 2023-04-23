cda6532f907d707c00b8b18514f8cf3d3d028977
Uchenna Obicheta <uobicheta@hotmail.com>
2023-04-23 14:01:31 -0400

    Updated routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
+// const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const loginRoutes = require('./loginRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
+const signupRoutes = require('./signupRoutes');
+const blogPostRoutes = require('./blogPostRoutes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+router.use('/home', homeRoutes);
+router.use('/login', loginRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/signup', signupRoutes);
+router.use('/blogPostRoutes', blogPostRoutes);
+router.use('/commentRoutes', commentRoutes);
+
 
 module.exports = router;
