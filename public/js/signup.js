327a9c9229072052db82be70ba4f7ae2ae922b00
Uchenna Obicheta <uobicheta@hotmail.com>
2023-04-23 17:21:27 -0400

    Implemented signup feature.

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,25 +1,17 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const firstname = document.querySelector('#inputFirstname4').value.trim();
-    const lastname = document.querySelector('#inputLastname4').value.trim();
-    const username = document.querySelector('#inputUsername4').value.trim();
-    const password = document.querySelector('#inputPassword4').value.trim();
-    const email = document.querySelector('#inputEmail4').value.trim();
-    const phone = document.querySelector('#inputPhone4').value.trim();
-    const address = document.querySelector('#inputAddress').value.trim();
-    const address2 = document.querySelector('#inputAddress2').value.trim();
-    const city = document.querySelector('#inputCity').value.trim();
-    const state = document.querySelector('#inputState').value.trim();
-    const zip = document.querySelector('#inputZip').value.trim();
-    const country = document.querySelector('#inputCountry').value.trim();
-    var membership = document.querySelector('#gridCheck').checked ? "premium" : "free";
-  
-    
+    const firstname = document.querySelector('#InputFirstName1').value.trim();
+    const lastname = document.querySelector('#InputLastName1').value.trim();
+    const username = document.querySelector('#InputUsername1').value.trim();
+    const password = document.querySelector('#InputPassword1').value.trim();
+    const email = document.querySelector('#InputEmail1').value.trim();
+   
+      
       console.log(JSON.stringify({username, password, email, firstname, lastname}));
 
     if (firstname && lastname && username && email && password) {
-      const response = await fetch('/users', { 
+      const response = await fetch('/signup', { 
         method: 'POST', 
         body: JSON.stringify({username, password, email, firstname, lastname}),
         headers: { 'Content-Type': 'application/json' },
